a67242873bad101a1468e4da0fbb9d8cae0b80a8
Jascha Sonntag <Jascha.Sonntag@weptech.de>
2024-08-14 14:48:41 +0200

    Tik Tac Toe

--- Form1.Designer.vb
+++ Form1.Designer.vb
@@ -22,18 +22,145 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
+        EinsEins = New Label()
+        EinsZwei = New Label()
+        DreiEins = New Label()
+        ZweiEins = New Label()
+        ZweiZwei = New Label()
+        EinsDrei = New Label()
+        ZweiDrei = New Label()
+        DreiZwei = New Label()
+        DreiDrei = New Label()
         SuspendLayout()
+        ' 
+        ' EinsEins
+        ' 
+        EinsEins.BorderStyle = BorderStyle.FixedSingle
+        EinsEins.Font = New Font("Segoe UI", 20F)
+        EinsEins.ForeColor = SystemColors.Control
+        EinsEins.Location = New Point(12, 9)
+        EinsEins.Name = "EinsEins"
+        EinsEins.Size = New Size(50, 50)
+        EinsEins.TabIndex = 0
+        EinsEins.TextAlign = ContentAlignment.MiddleCenter
+        ' 
+        ' EinsZwei
+        ' 
+        EinsZwei.BorderStyle = BorderStyle.FixedSingle
+        EinsZwei.Font = New Font("Segoe UI", 20F)
+        EinsZwei.ForeColor = SystemColors.Control
+        EinsZwei.Location = New Point(112, 9)
+        EinsZwei.Name = "EinsZwei"
+        EinsZwei.Size = New Size(50, 50)
+        EinsZwei.TabIndex = 1
+        EinsZwei.TextAlign = ContentAlignment.MiddleCenter
+        ' 
+        ' DreiEins
+        ' 
+        DreiEins.BorderStyle = BorderStyle.FixedSingle
+        DreiEins.Font = New Font("Segoe UI", 20F)
+        DreiEins.ForeColor = SystemColors.Control
+        DreiEins.Location = New Point(12, 209)
+        DreiEins.Name = "DreiEins"
+        DreiEins.Size = New Size(50, 50)
+        DreiEins.TabIndex = 2
+        DreiEins.TextAlign = ContentAlignment.MiddleCenter
+        ' 
+        ' ZweiEins
+        ' 
+        ZweiEins.BorderStyle = BorderStyle.FixedSingle
+        ZweiEins.Font = New Font("Segoe UI", 20F)
+        ZweiEins.ForeColor = SystemColors.Control
+        ZweiEins.Location = New Point(12, 109)
+        ZweiEins.Name = "ZweiEins"
+        ZweiEins.Size = New Size(50, 50)
+        ZweiEins.TabIndex = 3
+        ZweiEins.TextAlign = ContentAlignment.MiddleCenter
+        ' 
+        ' ZweiZwei
+        ' 
+        ZweiZwei.BorderStyle = BorderStyle.FixedSingle
+        ZweiZwei.Font = New Font("Segoe UI", 20F)
+        ZweiZwei.ForeColor = SystemColors.Control
+        ZweiZwei.Location = New Point(112, 109)
+        ZweiZwei.Name = "ZweiZwei"
+        ZweiZwei.Size = New Size(50, 50)
+        ZweiZwei.TabIndex = 4
+        ZweiZwei.TextAlign = ContentAlignment.MiddleCenter
+        ' 
+        ' EinsDrei
+        ' 
+        EinsDrei.BorderStyle = BorderStyle.FixedSingle
+        EinsDrei.Font = New Font("Segoe UI", 20F)
+        EinsDrei.ForeColor = SystemColors.Control
+        EinsDrei.Location = New Point(212, 9)
+        EinsDrei.Name = "EinsDrei"
+        EinsDrei.Size = New Size(50, 50)
+        EinsDrei.TabIndex = 5
+        EinsDrei.TextAlign = ContentAlignment.MiddleCenter
+        ' 
+        ' ZweiDrei
+        ' 
+        ZweiDrei.BorderStyle = BorderStyle.FixedSingle
+        ZweiDrei.Font = New Font("Segoe UI", 20F)
+        ZweiDrei.ForeColor = SystemColors.Control
+        ZweiDrei.Location = New Point(212, 109)
+        ZweiDrei.Name = "ZweiDrei"
+        ZweiDrei.Size = New Size(50, 50)
+        ZweiDrei.TabIndex = 6
+        ZweiDrei.TextAlign = ContentAlignment.MiddleCenter
+        ' 
+        ' DreiZwei
+        ' 
+        DreiZwei.BorderStyle = BorderStyle.FixedSingle
+        DreiZwei.Font = New Font("Segoe UI", 20F)
+        DreiZwei.ForeColor = SystemColors.Control
+        DreiZwei.Location = New Point(112, 209)
+        DreiZwei.Name = "DreiZwei"
+        DreiZwei.Size = New Size(50, 50)
+        DreiZwei.TabIndex = 7
+        DreiZwei.TextAlign = ContentAlignment.MiddleCenter
+        ' 
+        ' DreiDrei
+        ' 
+        DreiDrei.BorderStyle = BorderStyle.FixedSingle
+        DreiDrei.Font = New Font("Segoe UI", 20F)
+        DreiDrei.ForeColor = SystemColors.Control
+        DreiDrei.Location = New Point(212, 209)
+        DreiDrei.Name = "DreiDrei"
+        DreiDrei.Size = New Size(50, 50)
+        DreiDrei.TabIndex = 8
+        DreiDrei.TextAlign = ContentAlignment.MiddleCenter
         ' 
         ' Form1
         ' 
         AutoScaleDimensions = New SizeF(7F, 15F)
         AutoScaleMode = AutoScaleMode.Font
-        ClientSize = New Size(747, 409)
+        ClientSize = New Size(281, 273)
+        Controls.Add(DreiDrei)
+        Controls.Add(DreiZwei)
+        Controls.Add(ZweiDrei)
+        Controls.Add(EinsDrei)
+        Controls.Add(ZweiZwei)
+        Controls.Add(ZweiEins)
+        Controls.Add(DreiEins)
+        Controls.Add(EinsZwei)
+        Controls.Add(EinsEins)
         FormBorderStyle = FormBorderStyle.FixedSingle
         MaximizeBox = False
         Name = "Form1"
         Text = "Form1"
         ResumeLayout(False)
     End Sub
+
+    Friend WithEvents EinsEins As Label
+    Friend WithEvents EinsZwei As Label
+    Friend WithEvents DreiEins As Label
+    Friend WithEvents ZweiEins As Label
+    Friend WithEvents ZweiZwei As Label
+    Friend WithEvents EinsDrei As Label
+    Friend WithEvents ZweiDrei As Label
+    Friend WithEvents DreiZwei As Label
+    Friend WithEvents DreiDrei As Label
 
 End Class
--- Form1.vb
+++ Form1.vb
@@ -1,3 +1,74 @@
 ﻿Public Class Form1
+    Dim SpielerEins = "X"
+    Dim SpielerZwei = "O"
+    Dim EinsIstDran = True
+    Dim Felder As New List(Of Label)
+    Private Sub Feld_Click(sender As Object, e As EventArgs) Handles EinsEins.Click, EinsZwei.Click, EinsDrei.Click, ZweiEins.Click, ZweiZwei.Click, ZweiDrei.Click, DreiEins.Click, DreiZwei.Click, DreiDrei.Click
+        If EinsIstDran Then
+            sender.Text = SpielerEins
+        Else
+            sender.Text = SpielerZwei
+        End If
+        EinsIstDran = Not EinsIstDran
+        sender.Enabled = False
+        CheckEnde()
+    End Sub
 
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Felder.Add(EinsEins)
+        Felder.Add(EinsZwei)
+        Felder.Add(EinsDrei)
+        Felder.Add(ZweiEins)
+        Felder.Add(ZweiZwei)
+        Felder.Add(ZweiDrei)
+        Felder.Add(DreiEins)
+        Felder.Add(DreiZwei)
+        Felder.Add(DreiDrei)
+    End Sub
+
+    Private Sub Reset()
+        For Each Feld In Felder
+            Feld.Text = ""
+            Feld.Enabled = True
+        Next
+    End Sub
+
+    Private Sub CheckEnde()
+
+        If (EinsEins.Text = SpielerEins And EinsZwei.Text = SpielerEins And EinsDrei.Text = SpielerEins) Or
+            (ZweiEins.Text = SpielerEins And ZweiZwei.Text = SpielerEins And ZweiDrei.Text = SpielerEins) Or
+            (DreiEins.Text = SpielerEins And DreiZwei.Text = SpielerEins And DreiDrei.Text = SpielerEins) Or
+            (EinsEins.Text = SpielerEins And ZweiEins.Text = SpielerEins And DreiEins.Text = SpielerEins) Or
+            (EinsZwei.Text = SpielerEins And ZweiZwei.Text = SpielerEins And DreiZwei.Text = SpielerEins) Or
+            (EinsDrei.Text = SpielerEins And ZweiDrei.Text = SpielerEins And DreiDrei.Text = SpielerEins) Or
+            (EinsEins.Text = SpielerEins And ZweiZwei.Text = SpielerEins And DreiDrei.Text = SpielerEins) Or
+            (EinsDrei.Text = SpielerEins And ZweiZwei.Text = SpielerEins And DreiEins.Text = SpielerEins) Then
+            MsgBox("Spieler Eins hat gewonnen!")
+            Reset()
+        End If
+
+        If (EinsEins.Text = SpielerZwei And EinsZwei.Text = SpielerZwei And EinsDrei.Text = SpielerZwei) Or
+            (ZweiEins.Text = SpielerZwei And ZweiZwei.Text = SpielerZwei And ZweiDrei.Text = SpielerZwei) Or
+            (DreiEins.Text = SpielerZwei And DreiZwei.Text = SpielerZwei And DreiDrei.Text = SpielerZwei) Or
+            (EinsEins.Text = SpielerZwei And ZweiEins.Text = SpielerZwei And DreiEins.Text = SpielerZwei) Or
+            (EinsZwei.Text = SpielerZwei And ZweiZwei.Text = SpielerZwei And DreiZwei.Text = SpielerZwei) Or
+            (EinsDrei.Text = SpielerZwei And ZweiDrei.Text = SpielerZwei And DreiDrei.Text = SpielerZwei) Or
+            (EinsEins.Text = SpielerZwei And ZweiZwei.Text = SpielerZwei And DreiDrei.Text = SpielerZwei) Or
+            (EinsDrei.Text = SpielerZwei And ZweiZwei.Text = SpielerZwei And DreiEins.Text = SpielerZwei) Then
+            MsgBox("Spieler Zwei hat gewonnen!")
+            Reset()
+        End If
+
+        Dim Unentschieden = True
+        For Each Feld In Felder
+            If Feld.Text = "" Then
+                Unentschieden = False
+                Exit For
+            End If
+        Next
+        If Unentschieden Then
+            MsgBox("Unentschieden!")
+            Reset()
+        End If
+    End Sub
 End Class
