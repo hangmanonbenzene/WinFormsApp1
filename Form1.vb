a6850171e4d2ae062e53736099aed8f41077a123
Jascha Sonntag <Jascha.Sonntag@weptech.de>
2024-08-14 16:23:19 +0200

    Loops

--- Form1.vb
+++ Form1.vb
@@ -1,3 +1,29 @@
 ﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
 
+        Dim iBox1 As Integer = InputBox(" Bis Welche Nummer willst du zählen ? ")
+        Dim stBox2 As String = InputBox(" Gerade oder ungerade Zahlen ? ")
+        Dim iCount As Integer
+
+        If stBox2 = "ungerade" Then
+
+            iCount = 1
+            Do While iCount <= iBox1
+                MsgBox(iCount)
+
+                iCount = iCount + 2
+            Loop
+
+            'For iCount = 1 To iBox1 Step 2
+            '   MsgBox(iCount)
+            'Next
+
+        ElseIf stBox2 = "gerade" Then
+            For iCount = 2 To iBox1 Step 2
+                MsgBox(iCount)
+            Next
+        End If
+
+
+    End Sub
 End Class
