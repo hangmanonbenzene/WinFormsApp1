dba11358abb182b41637cefe7ea755498606f668
Jascha Sonntag <Jascha.Sonntag@weptech.de>
2024-08-14 11:52:42 +0200

    Rechner mit mehreren Checkboxen

--- Form1.Designer.vb
+++ Form1.Designer.vb
@@ -22,18 +22,119 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
+        Button1 = New Button()
+        CheckBox1 = New CheckBox()
+        CheckBox2 = New CheckBox()
+        CheckBox3 = New CheckBox()
+        CheckBox4 = New CheckBox()
+        Label1 = New Label()
+        NumericUpDown1 = New NumericUpDown()
+        NumericUpDown2 = New NumericUpDown()
+        CType(NumericUpDown1, ComponentModel.ISupportInitialize).BeginInit()
+        CType(NumericUpDown2, ComponentModel.ISupportInitialize).BeginInit()
         SuspendLayout()
+        ' 
+        ' Button1
+        ' 
+        Button1.Location = New Point(206, 45)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(75, 23)
+        Button1.TabIndex = 0
+        Button1.Text = "Rechnen"
+        Button1.UseVisualStyleBackColor = True
+        ' 
+        ' CheckBox1
+        ' 
+        CheckBox1.AutoSize = True
+        CheckBox1.Location = New Point(48, 128)
+        CheckBox1.Name = "CheckBox1"
+        CheckBox1.Size = New Size(31, 19)
+        CheckBox1.TabIndex = 1
+        CheckBox1.Text = "-"
+        CheckBox1.UseVisualStyleBackColor = True
+        ' 
+        ' CheckBox2
+        ' 
+        CheckBox2.AutoSize = True
+        CheckBox2.Location = New Point(48, 103)
+        CheckBox2.Name = "CheckBox2"
+        CheckBox2.Size = New Size(34, 19)
+        CheckBox2.TabIndex = 2
+        CheckBox2.Text = "+"
+        CheckBox2.UseVisualStyleBackColor = True
+        ' 
+        ' CheckBox3
+        ' 
+        CheckBox3.AutoSize = True
+        CheckBox3.Location = New Point(48, 153)
+        CheckBox3.Name = "CheckBox3"
+        CheckBox3.Size = New Size(31, 19)
+        CheckBox3.TabIndex = 3
+        CheckBox3.Text = "*"
+        CheckBox3.UseVisualStyleBackColor = True
+        ' 
+        ' CheckBox4
+        ' 
+        CheckBox4.AutoSize = True
+        CheckBox4.Location = New Point(48, 178)
+        CheckBox4.Name = "CheckBox4"
+        CheckBox4.Size = New Size(31, 19)
+        CheckBox4.TabIndex = 4
+        CheckBox4.Text = "/"
+        CheckBox4.UseVisualStyleBackColor = True
+        ' 
+        ' Label1
+        ' 
+        Label1.BorderStyle = BorderStyle.FixedSingle
+        Label1.Location = New Point(206, 74)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(75, 23)
+        Label1.TabIndex = 5
+        ' 
+        ' NumericUpDown1
+        ' 
+        NumericUpDown1.Location = New Point(48, 45)
+        NumericUpDown1.Name = "NumericUpDown1"
+        NumericUpDown1.Size = New Size(120, 23)
+        NumericUpDown1.TabIndex = 6
+        ' 
+        ' NumericUpDown2
+        ' 
+        NumericUpDown2.Location = New Point(48, 74)
+        NumericUpDown2.Name = "NumericUpDown2"
+        NumericUpDown2.Size = New Size(120, 23)
+        NumericUpDown2.TabIndex = 7
         ' 
         ' Form1
         ' 
         AutoScaleDimensions = New SizeF(7F, 15F)
         AutoScaleMode = AutoScaleMode.Font
         ClientSize = New Size(747, 409)
+        Controls.Add(NumericUpDown2)
+        Controls.Add(NumericUpDown1)
+        Controls.Add(Label1)
+        Controls.Add(CheckBox4)
+        Controls.Add(CheckBox3)
+        Controls.Add(CheckBox2)
+        Controls.Add(CheckBox1)
+        Controls.Add(Button1)
         FormBorderStyle = FormBorderStyle.FixedSingle
         MaximizeBox = False
         Name = "Form1"
         Text = "Form1"
+        CType(NumericUpDown1, ComponentModel.ISupportInitialize).EndInit()
+        CType(NumericUpDown2, ComponentModel.ISupportInitialize).EndInit()
         ResumeLayout(False)
+        PerformLayout()
     End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents CheckBox1 As CheckBox
+    Friend WithEvents CheckBox2 As CheckBox
+    Friend WithEvents CheckBox3 As CheckBox
+    Friend WithEvents CheckBox4 As CheckBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents NumericUpDown1 As NumericUpDown
+    Friend WithEvents NumericUpDown2 As NumericUpDown
 
 End Class
--- Form1.vb
+++ Form1.vb
@@ -1,3 +1,18 @@
 ﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim decErgebnis As Decimal
 
+        If CheckBox1.Checked Then
+            decErgebnis = NumericUpDown1.Value - NumericUpDown2.Value
+            Label1.Text = decErgebnis
+
+        ElseIf CheckBox2.Checked Then
+            decErgebnis = NumericUpDown1.Value + NumericUpDown2.Value
+            Label1.Text = decErgebnis
+        Else
+            Label1.Text = ""
+        End If
+
+
+    End Sub
 End Class
