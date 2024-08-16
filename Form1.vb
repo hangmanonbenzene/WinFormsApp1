2d6615871abd813de9c1a71726ea420b24485bf4
Jascha Sonntag <Jascha.Sonntag@weptech.de>
2024-08-16 11:08:06 +0200

    BlackJack

--- Form1.vb
+++ Form1.vb
@@ -1,12 +1,45 @@
 ﻿Public Class Form1
 
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+        Dim iZiehwert As Integer
+        Dim iAktwert As Integer
+        Dim stEingabe As String
 
 
 
+        Do
 
+            stEingabe = InputBox("Dein aktueller Wert ist " & iAktwert & vbCrLf & "Willst du eine weitere Zahl ziehen?")
 
+            If stEingabe.ToLower = "ja" Then
 
+                iZiehwert = neueZahl()
+                MsgBox("Du ziehst " & iZiehwert & ".")
 
+                iAktwert += iZiehwert
+
+                Select Case iAktwert
+
+                    Case Is > 21
+                        MsgBox("Verloren mit dem Wert " & iAktwert & ".")
+                        Exit Sub
+
+                    Case Is = 21
+                        MsgBox("Gewonnen mit dem Wert " & iAktwert & ".")
+                        Exit Sub
+
+                End Select
+
+            ElseIf stEingabe.ToLower = "nein" Then
+
+                MsgBox("Du hast das Spiel mit dem Wert " & iAktwert & " beendet.")
+
+            End If
+
+        Loop Until stEingabe.ToLower = "nein"
+
+    End Sub
 
 
 
@@ -16,4 +49,5 @@
     Public Function neueZahl() As Integer
         Return New Random().Next(1, 11)
     End Function
+
 End Class
