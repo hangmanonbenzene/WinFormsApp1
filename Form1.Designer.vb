a49828be2103a86948dfc1f3976ea9b8dc068138
Jascha Sonntag <Jascha.Sonntag@weptech.de>
2024-08-16 13:49:42 +0200

    Liste

--- Form1.Designer.vb
+++ Form1.Designer.vb
@@ -22,18 +22,67 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
+        Button1 = New Button()
+        eingabe = New TextBox()
+        Button2 = New Button()
+        stelle = New NumericUpDown()
+        CType(stelle, ComponentModel.ISupportInitialize).BeginInit()
         SuspendLayout()
+        ' 
+        ' Button1
+        ' 
+        Button1.Location = New Point(206, 67)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(132, 75)
+        Button1.TabIndex = 0
+        Button1.Text = "Hinzufügen"
+        Button1.UseVisualStyleBackColor = True
+        ' 
+        ' eingabe
+        ' 
+        eingabe.Location = New Point(75, 67)
+        eingabe.Name = "eingabe"
+        eingabe.Size = New Size(100, 23)
+        eingabe.TabIndex = 1
+        ' 
+        ' Button2
+        ' 
+        Button2.Location = New Point(75, 174)
+        Button2.Name = "Button2"
+        Button2.Size = New Size(263, 23)
+        Button2.TabIndex = 2
+        Button2.Text = "Liste ausgeben"
+        Button2.UseVisualStyleBackColor = True
+        ' 
+        ' stelle
+        ' 
+        stelle.Location = New Point(75, 119)
+        stelle.Maximum = New Decimal(New Integer() {9, 0, 0, 0})
+        stelle.Name = "stelle"
+        stelle.Size = New Size(100, 23)
+        stelle.TabIndex = 3
         ' 
         ' Form1
         ' 
         AutoScaleDimensions = New SizeF(7F, 15F)
         AutoScaleMode = AutoScaleMode.Font
         ClientSize = New Size(747, 409)
+        Controls.Add(stelle)
+        Controls.Add(Button2)
+        Controls.Add(eingabe)
+        Controls.Add(Button1)
         FormBorderStyle = FormBorderStyle.FixedSingle
         MaximizeBox = False
         Name = "Form1"
         Text = "Form1"
+        CType(stelle, ComponentModel.ISupportInitialize).EndInit()
         ResumeLayout(False)
+        PerformLayout()
     End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents eingabe As TextBox
+    Friend WithEvents Button2 As Button
+    Friend WithEvents stelle As NumericUpDown
 
 End Class
--- Form1.vb
+++ Form1.vb
@@ -1,3 +1,48 @@
 ﻿Public Class Form1
+    Dim astMeineListe(9) As String
 
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+        Dim stEingabe As String
+        Dim iStelle As Integer
+
+
+
+        stEingabe = eingabe.Text
+        iStelle = stelle.Value
+
+
+        astMeineListe(iStelle) = stEingabe
+
+
+
+
+
+
+
+    End Sub
+
+
+
+
+
+
+
+
+
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+
+
+        For count = 0 To astMeineListe.Length - 1
+            MsgBox(astMeineListe(count))
+        Next
+
+
+
+
+
+
+
+    End Sub
 End Class
