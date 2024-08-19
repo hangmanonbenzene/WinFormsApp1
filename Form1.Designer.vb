ef05ae9e38f976b6dc70a4e6682c5f073843b846
Jascha Sonntag <Jascha.Sonntag@weptech.de>
2024-08-19 15:59:03 +0200

    Würfelspiel

--- Form1.Designer.vb
+++ Form1.Designer.vb
@@ -22,18 +22,176 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
+        Label1 = New Label()
+        Label2 = New Label()
+        Label3 = New Label()
+        Label4 = New Label()
+        Label5 = New Label()
+        Label6 = New Label()
+        Label7 = New Label()
+        Label8 = New Label()
+        Label9 = New Label()
+        Label10 = New Label()
+        Label11 = New Label()
+        Label12 = New Label()
+        Button1 = New Button()
         SuspendLayout()
+        ' 
+        ' Label1
+        ' 
+        Label1.BorderStyle = BorderStyle.FixedSingle
+        Label1.Location = New Point(117, 92)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(25, 25)
+        Label1.TabIndex = 0
+        Label1.Text = "X"
+        Label1.TextAlign = ContentAlignment.MiddleCenter
+        ' 
+        ' Label2
+        ' 
+        Label2.BorderStyle = BorderStyle.FixedSingle
+        Label2.Location = New Point(148, 92)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(25, 25)
+        Label2.TabIndex = 1
+        Label2.TextAlign = ContentAlignment.MiddleCenter
+        ' 
+        ' Label3
+        ' 
+        Label3.BorderStyle = BorderStyle.FixedSingle
+        Label3.Location = New Point(179, 92)
+        Label3.Name = "Label3"
+        Label3.Size = New Size(25, 25)
+        Label3.TabIndex = 2
+        Label3.TextAlign = ContentAlignment.MiddleCenter
+        ' 
+        ' Label4
+        ' 
+        Label4.BorderStyle = BorderStyle.FixedSingle
+        Label4.Location = New Point(210, 92)
+        Label4.Name = "Label4"
+        Label4.Size = New Size(25, 25)
+        Label4.TabIndex = 3
+        Label4.TextAlign = ContentAlignment.MiddleCenter
+        ' 
+        ' Label5
+        ' 
+        Label5.BorderStyle = BorderStyle.FixedSingle
+        Label5.Location = New Point(210, 126)
+        Label5.Name = "Label5"
+        Label5.Size = New Size(25, 25)
+        Label5.TabIndex = 4
+        Label5.TextAlign = ContentAlignment.MiddleCenter
+        ' 
+        ' Label6
+        ' 
+        Label6.BorderStyle = BorderStyle.FixedSingle
+        Label6.Location = New Point(210, 162)
+        Label6.Name = "Label6"
+        Label6.Size = New Size(25, 25)
+        Label6.TabIndex = 5
+        Label6.TextAlign = ContentAlignment.MiddleCenter
+        ' 
+        ' Label7
+        ' 
+        Label7.BorderStyle = BorderStyle.FixedSingle
+        Label7.Location = New Point(179, 162)
+        Label7.Name = "Label7"
+        Label7.Size = New Size(25, 25)
+        Label7.TabIndex = 6
+        Label7.TextAlign = ContentAlignment.MiddleCenter
+        ' 
+        ' Label8
+        ' 
+        Label8.BorderStyle = BorderStyle.FixedSingle
+        Label8.Location = New Point(179, 200)
+        Label8.Name = "Label8"
+        Label8.Size = New Size(25, 25)
+        Label8.TabIndex = 7
+        Label8.TextAlign = ContentAlignment.MiddleCenter
+        ' 
+        ' Label9
+        ' 
+        Label9.BorderStyle = BorderStyle.FixedSingle
+        Label9.Location = New Point(148, 200)
+        Label9.Name = "Label9"
+        Label9.Size = New Size(25, 25)
+        Label9.TabIndex = 8
+        Label9.TextAlign = ContentAlignment.MiddleCenter
+        ' 
+        ' Label10
+        ' 
+        Label10.BorderStyle = BorderStyle.FixedSingle
+        Label10.Location = New Point(117, 200)
+        Label10.Name = "Label10"
+        Label10.Size = New Size(25, 25)
+        Label10.TabIndex = 9
+        Label10.TextAlign = ContentAlignment.MiddleCenter
+        ' 
+        ' Label11
+        ' 
+        Label11.BorderStyle = BorderStyle.FixedSingle
+        Label11.Location = New Point(117, 162)
+        Label11.Name = "Label11"
+        Label11.Size = New Size(25, 25)
+        Label11.TabIndex = 10
+        Label11.TextAlign = ContentAlignment.MiddleCenter
+        ' 
+        ' Label12
+        ' 
+        Label12.BorderStyle = BorderStyle.FixedSingle
+        Label12.Location = New Point(117, 126)
+        Label12.Name = "Label12"
+        Label12.Size = New Size(25, 25)
+        Label12.TabIndex = 11
+        Label12.TextAlign = ContentAlignment.MiddleCenter
+        ' 
+        ' Button1
+        ' 
+        Button1.Location = New Point(369, 117)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(162, 90)
+        Button1.TabIndex = 12
+        Button1.Text = "Würfeln"
+        Button1.UseVisualStyleBackColor = True
         ' 
         ' Form1
         ' 
         AutoScaleDimensions = New SizeF(7F, 15F)
         AutoScaleMode = AutoScaleMode.Font
         ClientSize = New Size(747, 409)
+        Controls.Add(Button1)
+        Controls.Add(Label12)
+        Controls.Add(Label11)
+        Controls.Add(Label10)
+        Controls.Add(Label9)
+        Controls.Add(Label8)
+        Controls.Add(Label7)
+        Controls.Add(Label6)
+        Controls.Add(Label5)
+        Controls.Add(Label4)
+        Controls.Add(Label3)
+        Controls.Add(Label2)
+        Controls.Add(Label1)
         FormBorderStyle = FormBorderStyle.FixedSingle
         MaximizeBox = False
         Name = "Form1"
         Text = "Form1"
         ResumeLayout(False)
     End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Label7 As Label
+    Friend WithEvents Label8 As Label
+    Friend WithEvents Label9 As Label
+    Friend WithEvents Label10 As Label
+    Friend WithEvents Label11 As Label
+    Friend WithEvents Label12 As Label
+    Friend WithEvents Button1 As Button
 
 End Class
--- Form1.vb
+++ Form1.vb
@@ -1,3 +1,47 @@
 ﻿Public Class Form1
+    Dim alblSpielfeld(11) As Label
+    Dim iSpielerposition As Integer = 0
+    Dim würfel As New Random
+
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+        Dim iWürfelergebnis As Integer
+
+        iWürfelergebnis = würfel.Next(1, 7)
+
+        MsgBox("Würfelergebnis war " & iWürfelergebnis)
+
+        alblSpielfeld(iSpielerposition).Text = ""
+
+        iSpielerposition = (iSpielerposition + iWürfelergebnis) Mod 12
+
+        alblSpielfeld(iSpielerposition).Text = "X"
+
+
+
+
+
+
+
+
+
+
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        alblSpielfeld(0) = Label1
+        alblSpielfeld(1) = Label2
+        alblSpielfeld(2) = Label3
+        alblSpielfeld(3) = Label4
+        alblSpielfeld(4) = Label5
+        alblSpielfeld(5) = Label6
+        alblSpielfeld(6) = Label7
+        alblSpielfeld(7) = Label8
+        alblSpielfeld(8) = Label9
+        alblSpielfeld(9) = Label10
+        alblSpielfeld(10) = Label11
+        alblSpielfeld(11) = Label12
+    End Sub
 
 End Class
