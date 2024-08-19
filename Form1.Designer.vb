2d18fb0869dfb285767b9ee918eaa89a8883bb40
Jascha Sonntag <Jascha.Sonntag@weptech.de>
2024-08-19 14:58:09 +0200

    Listen

--- Form1.Designer.vb
+++ Form1.Designer.vb
@@ -22,18 +22,55 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
+        NumericUpDown1 = New NumericUpDown()
+        TextBox1 = New TextBox()
+        Button1 = New Button()
+        CType(NumericUpDown1, ComponentModel.ISupportInitialize).BeginInit()
         SuspendLayout()
+        ' 
+        ' NumericUpDown1
+        ' 
+        NumericUpDown1.Location = New Point(76, 72)
+        NumericUpDown1.Maximum = New Decimal(New Integer() {9, 0, 0, 0})
+        NumericUpDown1.Name = "NumericUpDown1"
+        NumericUpDown1.Size = New Size(120, 23)
+        NumericUpDown1.TabIndex = 2
+        ' 
+        ' TextBox1
+        ' 
+        TextBox1.Location = New Point(76, 101)
+        TextBox1.Name = "TextBox1"
+        TextBox1.Size = New Size(120, 23)
+        TextBox1.TabIndex = 4
+        ' 
+        ' Button1
+        ' 
+        Button1.Location = New Point(230, 72)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(127, 52)
+        Button1.TabIndex = 5
+        Button1.Text = "Komplette Liste ausgeben"
+        Button1.UseVisualStyleBackColor = True
         ' 
         ' Form1
         ' 
         AutoScaleDimensions = New SizeF(7F, 15F)
         AutoScaleMode = AutoScaleMode.Font
         ClientSize = New Size(747, 409)
+        Controls.Add(Button1)
+        Controls.Add(TextBox1)
+        Controls.Add(NumericUpDown1)
         FormBorderStyle = FormBorderStyle.FixedSingle
         MaximizeBox = False
         Name = "Form1"
         Text = "Form1"
+        CType(NumericUpDown1, ComponentModel.ISupportInitialize).EndInit()
         ResumeLayout(False)
+        PerformLayout()
     End Sub
+
+    Friend WithEvents NumericUpDown1 As NumericUpDown
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Button1 As Button
 
 End Class
--- Form1.vb
+++ Form1.vb
@@ -1,3 +1,47 @@
-﻿Public Class Form1
+﻿Imports System.Diagnostics.Tracing
 
+Public Class Form1
+    Dim astListe As String() = {"Null", "Eins", "Zwei", "Drei", "Vier", "Fünf", "Sechs", "Sieben", "Acht", "Neun"}
+
+    Private Sub NumericUpDown1_ValueChanged() Handles NumericUpDown1.ValueChanged
+
+        Dim iPosition As Integer
+        Dim stListenText As String
+
+        iPosition = NumericUpDown1.Value
+        stListenText = astListe(iPosition)
+
+        TextBox1.Text = stListenText
+
+    End Sub
+
+    Private Sub TextBox1_TextChanged() Handles TextBox1.TextChanged
+
+        Dim iPosition As Integer
+        Dim stText As String
+
+        iPosition = NumericUpDown1.Value
+        stText = TextBox1.Text
+
+        astListe(iPosition) = stText
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+        Dim stText As String
+
+        For iStelle = 0 To astListe.Length - 1
+
+            stText = stText & astListe(iStelle) & vbCrLf
+
+        Next
+
+        MsgBox(stText)
+
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        NumericUpDown1_ValueChanged()
+    End Sub
 End Class
