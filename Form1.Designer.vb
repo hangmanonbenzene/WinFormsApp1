c36164d9b9ad5c7b30ad5f0145780e1955269c77
Jascha Sonntag <Jascha.Sonntag@weptech.de>
2024-08-19 08:08:19 +0200

    Test

--- Form1.Designer.vb
+++ Form1.Designer.vb
@@ -22,18 +22,56 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
+        ProgressBar1 = New ProgressBar()
+        btnStart = New Button()
+        Label1 = New Label()
         SuspendLayout()
+        ' 
+        ' ProgressBar1
+        ' 
+        ProgressBar1.Location = New Point(77, 175)
+        ProgressBar1.Maximum = 1000
+        ProgressBar1.Name = "ProgressBar1"
+        ProgressBar1.Size = New Size(350, 23)
+        ProgressBar1.Step = 1
+        ProgressBar1.TabIndex = 0
+        ' 
+        ' btnStart
+        ' 
+        btnStart.Location = New Point(268, 30)
+        btnStart.Name = "btnStart"
+        btnStart.Size = New Size(159, 95)
+        btnStart.TabIndex = 1
+        btnStart.Text = "Start"
+        btnStart.UseVisualStyleBackColor = True
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Location = New Point(433, 183)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(23, 15)
+        Label1.TabIndex = 2
+        Label1.Text = "0%"
         ' 
         ' Form1
         ' 
         AutoScaleDimensions = New SizeF(7F, 15F)
         AutoScaleMode = AutoScaleMode.Font
         ClientSize = New Size(747, 409)
+        Controls.Add(Label1)
+        Controls.Add(btnStart)
+        Controls.Add(ProgressBar1)
         FormBorderStyle = FormBorderStyle.FixedSingle
         MaximizeBox = False
         Name = "Form1"
         Text = "Form1"
         ResumeLayout(False)
+        PerformLayout()
     End Sub
+
+    Friend WithEvents ProgressBar1 As ProgressBar
+    Friend WithEvents btnStart As Button
+    Friend WithEvents Label1 As Label
 
 End Class
--- Form1.vb
+++ Form1.vb
@@ -1,3 +1,11 @@
 ﻿Public Class Form1
-
+    Private Sub btnStart_Click(sender As Object, e As EventArgs) Handles btnStart.Click
+        Dim rand As New Random
+        For i As Integer = 0 To 990
+            ProgressBar1.Value = i
+            Label1.Text = i.ToString().Substring(0, i.ToString().Length - 1) & "%"
+            Threading.Thread.Sleep(rand.Next(0, i * i / 2500))
+            Application.DoEvents()
+        Next
+    End Sub
 End Class
