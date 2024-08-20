8b87c3e873166ebf6dc5f676d9a7db01ed16fe24
Jascha Sonntag <Jascha.Sonntag@weptech.de>
2024-08-20 09:31:55 +0200

    Liste durchsuchen

--- Form1.vb
+++ Form1.vb
@@ -1,3 +1,34 @@
 ﻿Public Class Form1
+    Dim astObstkorb As String() = {"Apfel", "Birne", "Banane", "Orange", "Kiwi", "Mango", "Pfirsich", "Erdbeere", "Kirsche", "Zitrone"}
 
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+        Dim blIstInListe As Boolean
+        Dim stObstsuche As String
+
+
+        blIstInListe = False
+        stObstsuche = InputBox("Nach welchem Obst suchen?")
+
+
+        For iObstkorbposition = 0 To astObstkorb.Length - 1
+
+            If stObstsuche = astObstkorb(iObstkorbposition) Then
+
+                blIstInListe = True
+                Exit For
+
+            End If
+
+        Next
+
+
+        If blIstInListe = True Then
+            MsgBox("Obst ist im Korb")
+        Else
+            MsgBox("Obst ist nicht im Korb")
+        End If
+
+
+    End Sub
 End Class
