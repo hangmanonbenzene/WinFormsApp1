aac3880c30ccf3e74aa31298bf336f5bf3037ce9
Jascha Sonntag <Jascha.Sonntag@weptech.de>
2024-08-20 15:11:44 +0200

    Lösungen

--- Form1.Designer.vb
+++ Form1.Designer.vb
@@ -22,18 +22,115 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
+        Button1 = New Button()
+        Button2 = New Button()
+        Button3 = New Button()
+        Button4 = New Button()
+        Button5 = New Button()
+        Button6 = New Button()
+        Button7 = New Button()
+        Button8 = New Button()
         SuspendLayout()
+        ' 
+        ' Button1
+        ' 
+        Button1.Location = New Point(172, 56)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(171, 59)
+        Button1.TabIndex = 0
+        Button1.Text = "Button1"
+        Button1.UseVisualStyleBackColor = True
+        ' 
+        ' Button2
+        ' 
+        Button2.Location = New Point(172, 121)
+        Button2.Name = "Button2"
+        Button2.Size = New Size(171, 59)
+        Button2.TabIndex = 1
+        Button2.Text = "Button2"
+        Button2.UseVisualStyleBackColor = True
+        ' 
+        ' Button3
+        ' 
+        Button3.Location = New Point(172, 186)
+        Button3.Name = "Button3"
+        Button3.Size = New Size(171, 59)
+        Button3.TabIndex = 2
+        Button3.Text = "Button3"
+        Button3.UseVisualStyleBackColor = True
+        ' 
+        ' Button4
+        ' 
+        Button4.Location = New Point(172, 251)
+        Button4.Name = "Button4"
+        Button4.Size = New Size(171, 59)
+        Button4.TabIndex = 3
+        Button4.Text = "Button4"
+        Button4.UseVisualStyleBackColor = True
+        ' 
+        ' Button5
+        ' 
+        Button5.Location = New Point(349, 56)
+        Button5.Name = "Button5"
+        Button5.Size = New Size(171, 59)
+        Button5.TabIndex = 4
+        Button5.Text = "Button5"
+        Button5.UseVisualStyleBackColor = True
+        ' 
+        ' Button6
+        ' 
+        Button6.Location = New Point(349, 121)
+        Button6.Name = "Button6"
+        Button6.Size = New Size(171, 59)
+        Button6.TabIndex = 5
+        Button6.Text = "Button6"
+        Button6.UseVisualStyleBackColor = True
+        ' 
+        ' Button7
+        ' 
+        Button7.Location = New Point(349, 186)
+        Button7.Name = "Button7"
+        Button7.Size = New Size(171, 59)
+        Button7.TabIndex = 6
+        Button7.Text = "Button7"
+        Button7.UseVisualStyleBackColor = True
+        ' 
+        ' Button8
+        ' 
+        Button8.Location = New Point(349, 251)
+        Button8.Name = "Button8"
+        Button8.Size = New Size(171, 59)
+        Button8.TabIndex = 7
+        Button8.Text = "Button8"
+        Button8.UseVisualStyleBackColor = True
         ' 
         ' Form1
         ' 
         AutoScaleDimensions = New SizeF(7F, 15F)
         AutoScaleMode = AutoScaleMode.Font
         ClientSize = New Size(747, 409)
+        Controls.Add(Button8)
+        Controls.Add(Button7)
+        Controls.Add(Button6)
+        Controls.Add(Button5)
+        Controls.Add(Button4)
+        Controls.Add(Button3)
+        Controls.Add(Button2)
+        Controls.Add(Button1)
         FormBorderStyle = FormBorderStyle.FixedSingle
         MaximizeBox = False
         Name = "Form1"
         Text = "Form1"
         ResumeLayout(False)
     End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Button4 As Button
+    Friend WithEvents Button5 As Button
+    Friend WithEvents Button6 As Button
+    Friend WithEvents Button7 As Button
+    Friend WithEvents Button8 As Button
 
 End Class
--- Form1.vb
+++ Form1.vb
@@ -1,3 +1,130 @@
 ﻿Public Class Form1
 
+    Dim aiData() As Integer = {5, 7, 33, 22, 8}
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+        For iListe = 0 To aiData.Length - 1
+
+            MsgBox(aiData(iListe))
+
+        Next
+
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+
+        Dim stAusgabe As String = ""
+
+        For iCounter = 0 To aiData.Length - 1
+
+            stAusgabe += aiData(iCounter) & vbCrLf
+
+        Next
+
+        MsgBox(stAusgabe)
+
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+
+        Dim iErgebnis As Integer
+
+        For iCount = 0 To aiData.Length - 1
+
+            iErgebnis += aiData(iCount)
+
+        Next
+
+        MsgBox("Das Ergebnis ist " & iErgebnis)
+
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+
+        Dim iErgebnis As Integer
+        Dim iDurchschnitt As Integer
+
+        For iCount = 0 To aiData.Length - 1
+
+            iErgebnis += aiData(iCount)
+
+        Next
+
+        iDurchschnitt = iErgebnis / aiData.Length
+        MsgBox("Der Durchschnitt ist " & iDurchschnitt)
+
+    End Sub
+
+    Private Sub Button5_Click(sender As Object, e As EventArgs) Handles Button5.Click
+
+        Dim iErgebnis As Integer
+
+        For iCount = 0 To aiData.Length - 1
+
+            If aiData(iCount) > 20 Then
+
+                iErgebnis += aiData(iCount)
+
+            End If
+
+        Next
+
+        MsgBox("Die Summe aller Zahlen über 20 ist " & iErgebnis)
+
+    End Sub
+
+    Private Sub Button6_Click(sender As Object, e As EventArgs) Handles Button6.Click
+
+        Dim iErgebnis As Integer
+        iErgebnis = Integer.MinValue
+
+        For iCount = 0 To aiData.Length - 1
+
+            If iErgebnis < aiData(iCount) Then
+
+                iErgebnis = aiData(iCount)
+
+            End If
+
+        Next
+
+        MsgBox("Die grösste Zahl ist " & iErgebnis)
+
+    End Sub
+
+    Private Sub Button7_Click(sender As Object, e As EventArgs) Handles Button7.Click
+
+        Dim iErgebnis As Integer
+        iErgebnis = Integer.MaxValue
+
+        For iCount = 0 To aiData.Length - 1
+
+            If iErgebnis > aiData(iCount) Then
+
+                iErgebnis = aiData(iCount)
+
+            End If
+
+        Next
+
+        MsgBox("Die kleinste Zahl ist " & iErgebnis)
+
+    End Sub
+
+    Private Sub Button8_Click(sender As Object, e As EventArgs) Handles Button8.Click
+
+        Dim stAusgabe As String = ""
+
+        For iCounter = 0 To aiData.Length - 1
+
+            aiData(iCounter) = aiData(iCounter) * 2
+
+            stAusgabe += aiData(iCounter) & vbCrLf
+
+        Next
+
+        MsgBox(stAusgabe)
+
+    End Sub
 End Class
